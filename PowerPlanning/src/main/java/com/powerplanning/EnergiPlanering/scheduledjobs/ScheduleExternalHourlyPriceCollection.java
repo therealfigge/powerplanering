b6b8ef0c3ef1c6be2783df0f6ee327fd00b3d877
therealfigge <fredrik.strandberg@outlook.com>
2023-09-08 13:47:26 +0200

    Schedule work executes with Awaitility

--- PowerPlanning/src/main/java/com/powerplanning/EnergiPlanering/scheduledjobs/ScheduleExternalHourlyPriceCollection.java
+++ PowerPlanning/src/main/java/com/powerplanning/EnergiPlanering/scheduledjobs/ScheduleExternalHourlyPriceCollection.java
@@ -4,10 +4,10 @@
  */
 package com.powerplanning.EnergiPlanering.scheduledjobs;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -18,12 +18,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduleExternalHourlyPriceCollection {
     
-    private static final Logger log = LoggerFactory.getLogger(ScheduleExternalHourlyPriceCollection.class);
+    /*private static final Logger log = LoggerFactory.getLogger(ScheduleExternalHourlyPriceCollection.class);
     
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
     
     @Scheduled(fixedRate = 5000)
     public void reportCurrentTime() {
         log.info("The time is now {}", dateFormat.format(new Date()));
+    }*/
+    
+    @Scheduled(fixedRate=5000)
+    public void execute() {
+        System.out.println("Scheduled job is executed");
     }
 }
