a07a96774fb2bc60c1208a1decb19ae7a9e98d40
therealfigge <fredrik.strandberg@outlook.com>
2023-09-08 10:47:09 +0200

    Initial commit

---
+++ PowerPlanning/src/main/java/com/powerplanning/EnergiPlanering/EnergiPlaneringApplication.java
@@ -0,0 +1,13 @@
+package com.powerplanning.EnergiPlanering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergiPlaneringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergiPlaneringApplication.class, args);
+	}
+
+}
---
+++ PowerPlanning/src/test/java/com/powerplanning/EnergiPlanering/EnergiPlaneringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powerplanning.EnergiPlanering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergiPlaneringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
